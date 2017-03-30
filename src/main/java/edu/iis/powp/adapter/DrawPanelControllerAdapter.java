fe247bd58b9f8bb244698e93c691f5392c3c27bb
Marcin Krakowski <misckq@gmail.com>
2017-03-30 15:48:03 +0200

    Poprawiono wyświetlania tekstu

--- src/main/java/edu/iis/powp/adapter/DrawPanelControllerAdapter.java
+++ src/main/java/edu/iis/powp/adapter/DrawPanelControllerAdapter.java
@@ -9,11 +9,11 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter extends DrawPanelController implements IPlotter
+public class DrawPanelControllerAdapter extends DrawPanelController implements IPlotter
 { 
 	private int startX = 0, startY = 0;
 	
-    public MyAdapter() {
+    public DrawPanelControllerAdapter() {
 		super();
 	}
     
@@ -30,13 +30,13 @@ public class MyAdapter extends DrawPanelController implements IPlotter
         ILine line = LineFactory.getBasicLine();
     	line.setStartCoordinates(this.startX, this.startY);
         line.setEndCoordinates(x, y);
-
+        setPosition(x, y);
 		drawLine(line);
     }
 
     @Override
     public String toString()
     {
-        return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+        return "Buggy Simulator";
     }
 }
